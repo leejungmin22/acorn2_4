c18cec876d43939948a70c97223f8cb90bcd7454
kimhyeonkyung <khk5087@naver.com>
2020-02-18 18:09:22 +0900

    ggggg

--- Example2_4/src/MemberDto.java
+++ Example2_4/src/MemberDto.java
@@ -2,7 +2,10 @@
 public class MemberDto {
   private int num;
   ¤·¤·¤·¤·¤·
+<<<<<<< hyeonkyung
   private String name;
   ¤±¤±¤¤¤·¤±¤¤¤·
  ¤¡¤¡¤¡¤¡¤¡¤¡
+=======
+>>>>>>> bcde7e6 Revert "name ì‚­ì œ"
 }
