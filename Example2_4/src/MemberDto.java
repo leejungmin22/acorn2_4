dbc3082c510381f9c7082c1303da514e735dbde2
kimhyeonkyung <khk5087@naver.com>
2020-02-19 12:20:39 +0900

    Exhibition Spring project 추가

--- Example2_4/src/MemberDto.java
+++ Example2_4/src/MemberDto.java
@@ -1,11 +1,4 @@
 
 public class MemberDto {
-  private int num;
-  ¤·¤·¤·¤·¤·
-<<<<<<< hyeonkyung
-  private String name;
-  ¤±¤±¤¤¤·¤±¤¤¤·
- ¤¡¤¡¤¡¤¡¤¡¤¡
-=======
->>>>>>> bcde7e6 Revert "name ì‚­ì œ"
+
 }
