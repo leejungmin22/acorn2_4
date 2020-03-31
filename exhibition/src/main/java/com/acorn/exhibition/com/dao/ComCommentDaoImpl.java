913535c4951a7046f85ae4a43609776ca90fa5b0
SeulJi <SeulJi@seulji-pc>
2020-03-31 15:50:49 +0900

    community-댓글 오류수정

--- exhibition/src/main/java/com/acorn/exhibition/com/dao/ComCommentDaoImpl.java
+++ exhibition/src/main/java/com/acorn/exhibition/com/dao/ComCommentDaoImpl.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.acorn.exhibition.com.dto.ComCommentDto;
 
+@Repository
 public class ComCommentDaoImpl implements ComCommentDao{
 	@Autowired
 	private SqlSession session;
